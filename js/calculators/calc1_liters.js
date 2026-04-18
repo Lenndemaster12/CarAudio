7fc77ea974a66777b89e85712a42b256d3b90383
Lenndemaster12 <91090339+Lenndemaster12@users.noreply.github.com>
2026-04-19 01:39:56 +0200

    Add files via upload

---
+++ js/calculators/calc1_liters.js
@@ -0,0 +1,10 @@
+export default {
+  id: 1,
+  titel: "Enclosure Volume (liters)",
+  inputs: [
+    { id: "l", label: "Lengte (cm)" },
+    { id: "b", label: "Breedte (cm)" },
+    { id: "h", label: "Hoogte (cm)" }
+  ],
+  bereken: values => (values.l * values.b * values.h) / 1000
+};
