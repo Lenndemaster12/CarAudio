2386b2a45285e5a3bbc98dad37b40d09f6031465
Lenndemaster12 <91090339+Lenndemaster12@users.noreply.github.com>
2026-04-20 01:37:32 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,37 @@
+const CACHE_NAME = "customlowzzz-v2";
+const BASE = "/CarAudio"; // <-- jouw repo map
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("customlowzzz-cache").then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./detail.html",
-        "./style.css",
-        "./js/script.js",
-        "./js/detail.js",
-        "./manifest.json"
+        `${BASE}/`,
+        `${BASE}/index.html`,
+        `${BASE}/detail.html`,
+        `${BASE}/style.css`,
+        `${BASE}/js/script.js`,
+        `${BASE}/js/detail.js`,
+        `${BASE}/calculators.js`,
+        `${BASE}/manifest.json`,
+        `${BASE}/icons/icon-192.png`,
+        `${BASE}/icons/icon-512.png`,
+        `${BASE}/icons/splash-screen-1080x1920.png`
       ]);
     })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
