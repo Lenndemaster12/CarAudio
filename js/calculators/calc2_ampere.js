e6391ef38eaffc989d9275afb43d3b5e3cdddbdb
Lenndemaster12 <91090339+Lenndemaster12@users.noreply.github.com>
2026-04-20 01:04:33 +0200

    Delete js directory

--- js/calculators/calc2_ampere.js
+++
@@ -1,9 +0,0 @@
-export default {
-  id: 2,
-  titel: "Power draw (Ampere)",
-  inputs: [
-    { id: "watt", label: "Wattage" },
-    { id: "volt", label: "Voltage", type: "select", options: [12, 14, 14.4] }
-  ],
-  bereken: values => values.watt / values.volt
-};
--- js/calculators/calc3_gain.js
+++
@@ -1,9 +0,0 @@
-export default {
-  id: 3,
-  titel: "Gain Setting",
-  inputs: [
-    { id: "watt", label: "Wattage" },
-    { id: "ohm", label: "Ohm" }
-  ],
-  bereken: values => Math.sqrt(values.watt * values.ohm)
-};
