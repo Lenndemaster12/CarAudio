c34d68f65cfc27d8a80b544925100fa69006e276
Lenndemaster12 <91090339+Lenndemaster12@users.noreply.github.com>
2026-04-20 03:09:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,12 @@
-const CACHE_NAME = "customlowzzz-v3";
-const BASE = "/CarAudio"; // <-- jouw repo map
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        `${BASE}/`,
-        `${BASE}/index.html`,
-        `${BASE}/detail.html`,
-        `${BASE}/style.css`,
-        `${BASE}/js/script.js`,
-        `${BASE}/js/detail.js`,
-        `${BASE}/calculators.js`,
-        `${BASE}/manifest.json`,
-        `${BASE}/icons/icon-192.png`,
-        `${BASE}/icons/icon-512.png`,
-        `${BASE}/icons/splash-screen-1080x1920.png`
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
+self.addEventListener("activate", event => {
+  event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  self.registration.unregister();
 });
